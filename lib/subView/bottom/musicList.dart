5e939f4a63992a04d2e2b37227d67df2707a6fa8
skyloved <w2783858980@outlook.com>
2025-03-05 08:46:19 +0800

    添加音乐列表组件，使其具有一定播放功能

--- lib/subView/bottom/musicList.dart
+++ lib/subView/bottom/musicList.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/material.dart' as material;
 import 'package:provider/provider.dart';
+import 'package:window_manager/window_manager.dart';
 
 import '../../provider/bottomMusicPlayerProvider.dart';
 
@@ -16,8 +17,18 @@ class MusicList extends StatelessWidget {
     print("列表重建");
     print(
         "重建后的列表长度为${Provider.of<BottomMusicPlayerProvider>(context, listen: false).musicListNotifier.value.length}");
+    final musicListNotifier =
+        Provider.of<BottomMusicPlayerProvider>(context, listen: true)
+            .musicListNotifier;
     return Stack(
       children: [
+        Positioned(
+            child: DragToMoveArea(
+          child: SizedBox(
+            width: double.infinity,
+            height: 75,
+          ),
+        )),
         Positioned(
           right: 0,
           top: 50 + 25,
@@ -33,7 +44,51 @@ class MusicList extends StatelessWidget {
                 child: Transform.rotate(
                   angle: -pi,
                   //TODO  完成播放列表组件
-                  child: CustomScrollView(),
+                  child: CustomScrollView(
+                    slivers: [
+                      material.SliverAppBar(
+                        backgroundColor: Color.fromRGBO(45, 45, 56, 1),
+                        leading: IconButton(
+                            icon: Icon(FluentIcons.chrome_back_mirrored),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            }),
+                        title: Text("播放列表"),
+                        actions: [
+                          IconButton(
+                            icon: Icon(FluentIcons.delete),
+                            onPressed: () {
+                              Provider.of<BottomMusicPlayerProvider>(context,
+                                      listen: false)
+                                  .clearMusicList();
+                            },
+                          )
+                        ],
+                      ),
+                      SliverAnimatedList(
+                          initialItemCount: musicListNotifier.value.length,
+                          itemBuilder: (context, index, animation) {
+                            return SizeTransition(
+                              sizeFactor: animation,
+                              child: ListTile(
+                                title: Text(
+                                    musicListNotifier.value[index].musicName ??
+                                        "未知歌曲"),
+                                subtitle: Text(musicListNotifier
+                                        .value[index].musicArtist ??
+                                    "未知歌手"),
+                                onPressed: () {
+                                  Provider.of<BottomMusicPlayerProvider>(
+                                      context,
+                                      listen: false)
+                                    ..currentMusicIndexNotifier.value = index
+                                    ..playMusicAt(index);
+                                },
+                              ),
+                            );
+                          })
+                    ],
+                  ),
                 ),
               ),
             ),
